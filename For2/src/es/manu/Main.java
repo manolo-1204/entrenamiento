3ca0c823f11dfac81b1bbe19dbe5db19ecaf3783
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-01-17 10:40:04 +0100

    Añadido programa For2 que te permite sumar desde el 1 hasta el número que diga el ususario

---
+++ For2/src/es/manu/Main.java
@@ -0,0 +1,21 @@
+package es.manu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 0;
+        int n;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("¿Hasta que número quieres sumar? ");
+        n = entrada.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            num += (i + 1);
+            //System.out.println(i);
+        }
+        System.out.println("El resultado es " + num);
+    }
+}
