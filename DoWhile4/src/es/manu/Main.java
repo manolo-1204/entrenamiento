2e3e326468522250ca9ec7aa8c616a5553edcd9a
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-02-04 13:46:33 +0100

    el dowhile 4

---
+++ DoWhile4/src/es/manu/Main.java
@@ -0,0 +1,22 @@
+package es.manu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero, count = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i ++) {
+            System.out.println("Numero " + i + ":");
+            System.out.println("====================================================");
+            do {
+                System.out.print("Dame un numerito entre 1 y 100: ");
+                numero = entrada.nextInt();
+            } while (numero < 1 || numero > 100);
+            System.out.println();
+        }
+
+    }
+}
