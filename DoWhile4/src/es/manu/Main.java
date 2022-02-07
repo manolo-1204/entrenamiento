68af7d338e5990cf70534a64e482b310459ee6d6
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-02-07 10:40:16 +0100

    notas

--- DoWhile4/src/es/manu/Main.java
+++ DoWhile4/src/es/manu/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int numero;
+        int numero, notas;
         Scanner entrada = new Scanner(System.in);
 
         for (int i = 1; i <= 10; i ++) {
--- Notas/src/es/manu/Main.java
+++ Notas/src/es/manu/Main.java
@@ -1,8 +1,33 @@
 package es.manu;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int numero, notas, aprobados = 0, suspensos = 0, media = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("¿Cuantas notas quieres introducir? ");
+        notas = entrada.nextInt();
+
+        for (int i = 1; i <= notas; i ++) {
+            //System.out.println("Nota " + i + ":");
+            //System.out.println("--------------");
+            do {
+                System.out.print("Nota " + i + ": ");
+                numero = entrada.nextInt();
+            } while (numero < 0 || numero > 10);
+            if (numero >= 5) {
+                aprobados++;
+            } else {
+                suspensos++;
+            }
+            media += numero;
+            System.out.println();
+        }
+        System.out.println("Nº de aprobados: " + aprobados);
+        System.out.println("Nº de suspensos: " + suspensos);
+        System.out.println("Media: " + (float) media/notas);
     }
 }
