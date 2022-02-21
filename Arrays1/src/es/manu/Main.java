6a07eac577802ad72ccb106dd0b24088537e73ef
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-02-21 10:39:39 +0100

    cuidao

---
+++ Arrays1/src/es/manu/Main.java
@@ -0,0 +1,28 @@
+package es.manu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int elementos[] = new int[10];
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i = 0; i < elementos.length; i++) {
+            System.out.print("Dame un numerito (" + (i + 1) + "): ");
+            elementos[i] = entrada.nextInt();
+        }
+        System.out.println();
+
+        System.out.println("Estos son los números que me has dado: ");
+        for (int i = 0; i < elementos.length; i++) {
+            if (elementos.length - i != 1) {
+                System.out.print("y " +elementos[i]);
+            } else if (){
+                System.out.print(elementos[i]);
+            }
+        }
+
+        entrada.close();
+    }
+}
